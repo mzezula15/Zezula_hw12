6fc3e60ea7999e1c2cd1bbca69617d7e3f88c5c8
Mateusz Zezula <mateuszzezula@Mateuszs-MacBook-Pro.local>
2018-12-01 12:03:38 -0500

    First commit for hw12

---
+++ hw12.R
@@ -0,0 +1,8 @@
+setwd("/users/mateuszzezula/Box/1. First Semester/Programming for Data Science/Homework/")
+
+
+# Find a non-english newspaper with an api. Le Monde is one choice. Write
+# functions that search the newspaper in some way, and use Google Cloud to
+# translate the text into English and produce mp3 files of newspaper text read
+# in English. Your code should allow the user to listen to the mp3 files from
+# within R.
